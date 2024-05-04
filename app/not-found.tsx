c812d3f68f3e758ec33ff01599a29a66f7d424f1
Fernando Moreira <nandomoreira.me@gmail.com>
2024-05-04 10:32:02 -0300

    feat: add not-found page and shadcn custom theme

---
+++ app/not-found.tsx
@@ -0,0 +1,35 @@
+import { cn } from '@/lib/utils';
+import { Button } from '@components';
+import { Metadata } from 'next';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: `NotFound page`,
+  description: `This is a description for NotFound page!`,
+};
+
+export default function NotFoundPage() {
+  return (
+    <div
+      className={cn(
+        'flex flex-col items-center justify-between md:min-h-screen pt-36 px-8 md:pt-0 md:px-12 lg:px-24',
+      )}
+    >
+      <div className="max-w-screen-md m-auto relative text-center">
+        <h1
+          className={cn(
+            'text-4xl md:text-6xl lg:text-8xl font-sans font-extrabold mb-4',
+          )}
+        >
+          404
+        </h1>
+        <h2 className="font-heading leading-tight mb-6 text-2xl sm:text-3xl font-medium">
+          Oops! A página que vocês acessou não foi encontrada ou não existe.
+        </h2>
+        <Button variant={'outline'} asChild>
+          <Link href="/">Voltar para home</Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
