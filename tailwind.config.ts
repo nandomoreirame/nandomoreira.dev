0269fb182e8b7de82a327104bd57daff55c2e312
Fernando Moreira <nandomoreira.me@gmail.com>
2024-05-09 12:00:06 -0300

    feat: add avatar new rounded

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -60,6 +60,7 @@ const config = {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
+        blob: 'var(--radius-blob)',
       },
       keyframes: {
         'accordion-down': {
